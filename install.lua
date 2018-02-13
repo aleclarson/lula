f295b8b762c798d8c636c072828e38d888ad1965
aleclarson <alec.stanford.larson@gmail.com>
2018-02-12 20:02:04 -0500

    move some functions to util.lua

--- install.lua
+++ install.lua
@@ -2,48 +2,13 @@ local fs = require('luarocks.fs')
 local cfg = require('luarocks.cfg')
 local make = require('luarocks.make')
 local path = require('luarocks.path')
+local util = require('util')
 local semver = require('semver')
 
 local LUA_VERSION = _VERSION:gsub("Lua (%d%.+)", "%1")
 
-local function path_exists(path)
-  local fh = io.open(path, 'r')
-  if fh then return fh:close() else return false end
-end
-
-local function try_path(root_dir, dir)
-  local path = root_dir .. '/' .. dir
-  if path_exists(path) then
-    return path
-  end
-end
-
 local function get_rockspec_dir(root_dir)
-  return try_path(root_dir, 'rockspecs') or try_path(root_dir, 'rockspec')
-end
-
-local function read_dir(dir, opts)
-  local cmd = 'ls'
-
-  local all = opts and opts.all == true
-  if all then cmd = cmd .. ' -a' end
-
-  local fh = io.popen(cmd .. ' "' .. dir .. '"')
-  local paths = fh:lines()
-
-  if all then
-    -- Ignore . and .. paths
-    while true do
-      local path = paths()
-      if path ~= '.' and path ~= '..' then break end
-    end
-  end
-
-  return function()
-    local path = paths()
-    if path then return path end
-    fh:close()
-  end
+  return util.try_path(root_dir, 'rockspecs') or util.try_path(root_dir, 'rockspec')
 end
 
 return function(cwd)
@@ -55,7 +20,7 @@ return function(cwd)
     if spec_dir == nil then return end
 
     local spec, latest_version
-    for file in read_dir(spec_dir) do
+    for file in util.read_dir(spec_dir) do
       local _, v = path.parse_name(file)
       local ok, version = pcall(semver, v)
       if ok then
---
+++ util.lua
@@ -0,0 +1,43 @@
+
+local function read_dir(dir, opts)
+  local cmd = 'ls'
+
+  local all = opts and opts.all == true
+  if all then cmd = cmd .. ' -a' end
+
+  local fh = io.popen(cmd .. ' "' .. dir .. '"')
+  local paths = fh:lines()
+
+  if all then
+    -- Ignore . and .. paths
+    while true do
+      local path = paths()
+      if path ~= '.' and path ~= '..' then break end
+    end
+  end
+
+  return function()
+    local path = paths()
+    if path then return path end
+    fh:close()
+  end
+end
+
+local function path_exists(path)
+  local fh = io.open(path, 'r')
+  if fh then return fh:close() else return false end
+end
+
+local function try_path(root_dir, dir)
+  local path = root_dir .. '/' .. dir
+  if path_exists(path) then
+    return path
+  end
+end
+
+return {
+  read_dir = read_dir,
+  path_exists = path_exists,
+  try_path = try_path,
+}
+
